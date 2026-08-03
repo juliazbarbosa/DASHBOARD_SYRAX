ee7724cd3e54988187f9adb98ab46cfc58957e0f
juliazbarbosa <127155774+juliazbarbosa@users.noreply.github.com>
2026-08-03 16:43:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let myChart = document.getElementById("myChart").getContext('2d');
-
 let myChart2 = document.getElementById("myChart2").getContext('2d');
 
-mychart = new Chart(myChart, {
+const paletaCores = ['#ff780a', '#2e9e5b', '#ffb84d', '#4c9aff', '#a78bfa', '#f472b6'];
+
+new Chart(myChart, {
     type: 'bar',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: ['Celulares', 'Notebooks', 'Tablets', 'Acessórios', 'Relógios', 'Áudio'],
       datasets: [{
-        label: '# of Votes',
+        label: 'Quantidade de vendas',
         data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: paletaCores,
         borderWidth: 1
       }]
     },
@@ -17,29 +19,25 @@ mychart = new Chart(myChart, {
         maintainAspectRatio: false,
         scales: {
             y: {
-            beginAtZero: true
+                beginAtZero: true
             }
         }
     }
 });
 
-mychart2 = new Chart(myChart2, {
+new Chart(myChart2, {
     type: 'doughnut',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: ['Celulares', 'Notebooks', 'Tablets', 'Acessórios', 'Relógios', 'Áudio'],
       datasets: [{
-        label: '# of Votes',
+        label: 'Quantidade de vendas',
         data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: paletaCores,
         borderWidth: 1
       }]
     },
     options: {
         responsive: true,
-        maintainAspectRatio: false,
-        scales: {
-            y: {
-            beginAtZero: true
-            }
-        }
+        maintainAspectRatio: false
     }
 });
